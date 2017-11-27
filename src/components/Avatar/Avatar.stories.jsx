f3f080de557672141f1370ed88443636af689798
Danny Eck <daniel.eck@accenture.com>
2017-11-27 09:02:53 -0800

    Add knobs to Avatar story

--- src/components/Avatar/Avatar.stories.jsx
+++ src/components/Avatar/Avatar.stories.jsx
@@ -3,22 +3,32 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
+import { withKnobs, text, boolean, number, select } from '@storybook/addon-knobs';
 
 import Avatar from './Avatar';
 
 const imgUrl = 'http://gentlemint-media.s3.amazonaws.com/images/2012/02/16/375c182c.jpg.505x650_q85.jpg'
 
-storiesOf('Avatar', module)
-  .add('things', () => (
+storiesOf('Avatars', module)
+  .addDecorator(withKnobs)
+
+  /**
+   *
+   */
+  .add('Avatar', () => (
+    <Avatar
+      size={select('Size', ['sm', 'md', 'lg'], 'md')}
+      imgUrl={text('imgUrl', '')}
+    />
+  ))
+
+  /**
+   *
+   */
+  .add('sizes', () => (
     <div>
-      <h2>Avatars</h2>
-      <Avatar />
-      <Avatar imgUrl={imgUrl} />
-
-      <h3>Sizes</h3>
-
       <p>Small</p>
-      Inline <Avatar size="sm" />
+      <Avatar size="sm" />
       <Avatar size="sm" imgUrl={imgUrl} />
       <p>Medium (default)</p>
       <Avatar />
