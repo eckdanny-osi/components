07deba864dcfc2142e30fb6ba86c0b95bdc286aa
Danny Eck <daniel.eck@accenture.com>
2017-11-27 10:53:21 -0800

    Add active Card styles

--- src/components/Card/Card.stories.jsx
+++ src/components/Card/Card.stories.jsx
@@ -33,12 +33,22 @@ storiesOf('Card', module)
    */
   .add('Action Cards', () => (
     <Card
-      header={<h3>Header</h3>}
+      header={(
+        <div className="clearfix">
+          <div style={{
+            display: 'inline-block',
+            padding: '6px 0'  // $padding-base-vertical
+          }}>Header</div>
+          <Button bsStyle="secondary" className="pull-right">Edit</Button>
+        </div>
+      )}
       footer={(
-        <ButtonToolbar>
-          <Button bsStyle="success">Hooray!</Button>
-          <Button bsStyle="danger">Oh nos!</Button>
-        </ButtonToolbar>
+        <div className="clearfix">
+          <ButtonToolbar className="pull-right">
+            <Button bsStyle="secondary">Cancel</Button>
+            <Button bsStyle="primary">Save</Button>
+          </ButtonToolbar>
+        </div>
       )}
     >
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus venenatis auctor. Ut vestibulum aliquet fermentum. Curabitur a imperdiet risus. Aenean vulputate, est bibendum faucibus consequat, est tortor consectetur dolor, ac egestas arcu ligula sed metus. Aenean est metus, tincidunt cursus velit eu, lacinia luctus lorem. Etiam fermentum nibh ultrices facilisis bibendum. Integer eu luctus mi. Cras scelerisque diam in metus molestie aliquam. Nunc commodo metus sed est vulputate fermentum. Phasellus at nunc sit amet elit maximus euismod. Phasellus vitae imperdiet erat, cursus pretium orci. Maecenas nec sapien elit. Etiam congue risus non enim volutpat, vitae euismod enim condimentum. Sed malesuada rutrum lectus et dignissim.</p>
