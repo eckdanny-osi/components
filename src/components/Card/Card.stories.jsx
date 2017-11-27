3809f09aa01ffae816d3a508fba8e69aa2b74df4
Danny Eck <daniel.eck@accenture.com>
2017-11-27 11:07:59 -0800

    Add Action card story

--- src/components/Card/Card.stories.jsx
+++ src/components/Card/Card.stories.jsx
@@ -6,6 +6,87 @@ import { linkTo } from '@storybook/addon-links';
 
 import { Card, Button, ButtonToolbar } from '../';
 
+
+class ActiveCardDemo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      scope: 'read'
+    };
+    this.setScope = this.setScope.bind(this);
+  }
+  setScope(scope) {
+    this.setState({scope});
+  }
+  _renderReadView() {
+    return (
+      <Card
+        header={(
+          <div className="clearfix">
+            <div style={{
+              display: 'inline-block',
+              padding: '6px 0'  // $padding-base-vertical
+            }}>Header</div>
+            <Button
+              bsStyle="secondary"
+              className="pull-right"
+              onClick={() => this.setScope('edit')}
+              >Edit</Button>
+          </div>
+        )}
+      >
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus venenatis auctor. Ut vestibulum aliquet fermentum. Curabitur a imperdiet risus. Aenean vulputate, est bibendum faucibus consequat, est tortor consectetur dolor, ac egestas arcu ligula sed metus. Aenean est metus, tincidunt cursus velit eu, lacinia luctus lorem. Etiam fermentum nibh ultrices facilisis bibendum. Integer eu luctus mi. Cras scelerisque diam in metus molestie aliquam. Nunc commodo metus sed est vulputate fermentum. Phasellus at nunc sit amet elit maximus euismod. Phasellus vitae imperdiet erat, cursus pretium orci. Maecenas nec sapien elit. Etiam congue risus non enim volutpat, vitae euismod enim condimentum. Sed malesuada rutrum lectus et dignissim.</p>
+      </Card>
+    );
+  }
+
+  _renderEditView() {
+    return (
+      <Card
+        className="panel-active"
+        header={(
+          <div className="clearfix">
+            <div style={{
+              display: 'inline-block',
+              padding: '6px 0'  // $padding-base-vertical
+            }}>Header</div>
+          </div>
+        )}
+        footer={(
+          <div className="clearfix">
+            <ButtonToolbar className="pull-right">
+              <Button
+                bsStyle="secondary"
+                onClick={() => this.setScope('read')}
+                >Cancel</Button>
+              <Button
+                bsStyle="primary"
+                onClick={() => this.setScope('read')}
+                >Save</Button>
+            </ButtonToolbar>
+          </div>
+        )}
+      >
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus venenatis auctor. Ut vestibulum aliquet fermentum. Curabitur a imperdiet risus. Aenean vulputate, est bibendum faucibus consequat, est tortor consectetur dolor, ac egestas arcu ligula sed metus. Aenean est metus, tincidunt cursus velit eu, lacinia luctus lorem. Etiam fermentum nibh ultrices facilisis bibendum. Integer eu luctus mi. Cras scelerisque diam in metus molestie aliquam. Nunc commodo metus sed est vulputate fermentum. Phasellus at nunc sit amet elit maximus euismod. Phasellus vitae imperdiet erat, cursus pretium orci. Maecenas nec sapien elit. Etiam congue risus non enim volutpat, vitae euismod enim condimentum. Sed malesuada rutrum lectus et dignissim.</p>
+      </Card>
+    );
+  }
+
+  render() {
+    switch (this.state.scope) {
+      case 'read': {
+        return this._renderReadView();
+      }
+      case 'edit': {
+        return this._renderEditView();
+      }
+      default:
+        return <div>OH NO, i broke</div>
+    }
+  }
+}
+
+
 storiesOf('Card', module)
 
   /**
@@ -32,25 +113,5 @@ storiesOf('Card', module)
    *
    */
   .add('Action Cards', () => (
-    <Card
-      header={(
-        <div className="clearfix">
-          <div style={{
-            display: 'inline-block',
-            padding: '6px 0'  // $padding-base-vertical
-          }}>Header</div>
-          <Button bsStyle="secondary" className="pull-right">Edit</Button>
-        </div>
-      )}
-      footer={(
-        <div className="clearfix">
-          <ButtonToolbar className="pull-right">
-            <Button bsStyle="secondary">Cancel</Button>
-            <Button bsStyle="primary">Save</Button>
-          </ButtonToolbar>
-        </div>
-      )}
-    >
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus venenatis auctor. Ut vestibulum aliquet fermentum. Curabitur a imperdiet risus. Aenean vulputate, est bibendum faucibus consequat, est tortor consectetur dolor, ac egestas arcu ligula sed metus. Aenean est metus, tincidunt cursus velit eu, lacinia luctus lorem. Etiam fermentum nibh ultrices facilisis bibendum. Integer eu luctus mi. Cras scelerisque diam in metus molestie aliquam. Nunc commodo metus sed est vulputate fermentum. Phasellus at nunc sit amet elit maximus euismod. Phasellus vitae imperdiet erat, cursus pretium orci. Maecenas nec sapien elit. Etiam congue risus non enim volutpat, vitae euismod enim condimentum. Sed malesuada rutrum lectus et dignissim.</p>
-    </Card>
+    <ActiveCardDemo />
   ));
