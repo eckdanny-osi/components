251083019b415323a65fcadb9e5748da79a25878
Danny Eck <daniel.eck@accenture.com>
2017-11-28 12:12:02 -0800

    WiP

--- src/components/RelationshipGraph/RelationshipGraph.jsx
+++ src/components/RelationshipGraph/RelationshipGraph.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import * as d3 from 'd3';
 import { timeFormat } from 'd3-time-format';
 import { scaleLinear } from 'd3-scale';
+import { forceSimulation } from 'd3-force';
 
 const propTypes = {};
 
@@ -40,26 +41,156 @@ export class LineChart extends React.Component {
     }
   }
 
+  componentDidMount() {
+    d3.select(this.ref)
+    .append("circle")
+    .attr("r", 5)
+    .attr("cx", this.props.width / 2)
+    .attr("cy", this.props.height / 2)
+    .attr("fill", "red");
+  }
+
   render() {
+    return (
+      <svg
+        className="container"
+        ref={(ref) => this.ref = ref}
+        width={this.props.width}
+        height={this.props.height}
+      />
+    );
+  }
 
-    const margin = {top: 5, right: 50, bottom: 20, left: 50};
-    const w = this.state.width - (margin.left + margin.right);
-    const h = this.props.height - (margin.top + margin.bottom);
+}
 
-    const transform='translate(' + margin.left + ',' + margin.top + ')';
+
+export class D3Thing extends React.Component {
+  // ref: HTMLDivElement;
+  // simulation: any;
+
+  constructor(props) {
+    super(props);
+    this.simulation = d3.forceSimulation()
+      .force("link", d3.forceLink().id(function(d) {
+        return d.id;
+      }))
+      .force("charge", d3.forceManyBody().strength(-100))
+      .force("center", d3.forceCenter(this.props.width / 2, this.props.height / 2))
+      .nodes(this.props.graph.nodes);
+
+    this.simulation.force("link").links(this.props.graph.links);
+  }
+
+  componentDidMount() {
+    const node = d3.select(".nodes").selectAll("circle");
+    const link = d3.select(".links").selectAll("line");
+
+    this.simulation.nodes(this.props.graph.nodes).on("tick", ticked);
+
+    function ticked() {
+      link
+        .attr("x1", function(d) {
+          return d.source.x;
+        })
+        .attr("y1", function(d) {
+          return d.source.y;
+        })
+        .attr("x2", function(d) {
+          return d.target.x;
+        })
+        .attr("y2", function(d) {
+          return d.target.y;
+        });
+
+      node
+        .attr("cx", function(d) {
+          return d.x;
+        })
+        .attr("cy", function(d) {
+          return d.y;
+        });
+    }
+  }
+
+  render() {
+    const { width, height, graph } = this.props;
 
     return (
-      <div>
-        <h3>Hello from d3</h3>
-        <svg id={this.props.chartId} width={this.state.width} height={this.props.height}>
-          <g transform={transform}>
-            <circle cx="25" cy="25" r="25" fill="purple" />
-            <circle cx="75" cy="100" r="25" fill="purple" />
-            <circle cx="25" cy="25" r="25" fill="purple" />
-          </g>
-        </svg>
-      </div>
+      <svg className="container" width={width} height={height}>
+        <Links links={graph.links}/>
+        <Nodes nodes={graph.nodes} simulation={this.simulation}/>
+      </svg>
     );
+  }
+}
+
+class Links extends React.Component {
+  // ref: SVGGElement;
+
+  componentDidMount() {
+    const context = d3.select(this.ref);
+    context
+      .selectAll("line")
+      .data(this.props.links)
+      .enter().append("line")
+      .attr("stroke-width", function(d) {
+        return Math.sqrt(d.value);
+      });
+  }
+
+  render() {
+    return <g className="links" ref={(ref) => this.ref = ref}/>;
+  }
+}
+
+class Nodes extends React.Component {
+  // ref: SVGGElement;
+
+  componentDidMount() {
+    const context = d3.select(this.ref);
+    const simulation = this.props.simulation;
+    const color = d3.scaleOrdinal(d3.schemeCategory20);
+
+    context.selectAll("circle")
+      .data(this.props.nodes)
+      .enter().append("circle")
+      .attr("r", 5)
+      .attr("fill", function(d) {
+        return color(d.group.toString());
+      })
+      .call(d3.drag()
+          .on("start", onDragStart)
+          .on("drag", onDrag)
+          .on("end", onDragEnd))
+      .append("title")
+        .text(function(d) {
+          return d.id;
+        });
+
+    function onDragStart(d) {
+      if (!d3.event.active) {
+        simulation.alphaTarget(0.3).restart();
+      }
+      d.fx = d.x;
+      d.fy = d.y;
+    }
+
+    function onDrag(d) {
+      d.fx = d3.event.x;
+      d.fy = d3.event.y;
+    }
+
+    function onDragEnd(d) {
+      if (!d3.event.active) {
+        simulation.alphaTarget(0);
+      }
+      d.fx = null;
+      d.fy = null;
+    }
+  }
+
+  render() {
+    return <g className="nodes" ref={(ref) => this.ref = ref}/>;
   }
 }
 
