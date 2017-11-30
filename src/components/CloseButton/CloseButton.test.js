5018757c80ad09982db2ba3ed6710ed65e06f9c6
Danny Eck <daniel.eck@accenture.com>
2017-11-30 10:34:08 -0800

    Add CloseButton tests

---
+++ src/components/CloseButton/CloseButton.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+
+import CloseButton from './CloseButton';
+import { Icon } from '../';
+
+describe('<CloseButton />', () => {
+  it('should render', () => {
+    expect(() => shallow(<CloseButton />)).not.toThrow();
+  });
+  it('should render a button', () => {
+    expect(shallow(<CloseButton />).type()).toBe('button');
+  });
+  it('should render an Icon', () => {
+    const wrapper = shallow(<CloseButton />);
+    expect(wrapper.find(Icon).length).toBe(1);
+    expect(wrapper.find(Icon).first().props().name).toEqual(jasmine.any(String));
+  });
+  it('renders the label prop', () => {
+    const wrapper = shallow((<CloseButton label="my label" />));
+    expect(wrapper.text()).toContain('my label');
+  });
+  it('responds to click events', () => {
+    const onClick = jasmine.createSpy('onClick');
+    const wrapper = shallow(<CloseButton onClick={onClick} />);
+    wrapper.find('button').simulate('click');
+    expect(onClick).toHaveBeenCalled();
+  });
+});
