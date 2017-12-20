5693bc199dbb443ef76dde7adac17c52a5a8c991
Danny Eck <daniel.eck@accenture.com>
2017-12-20 13:01:50 -0800

    WiP

--- src/components/FormGroup/Validation.stories.js
+++ src/components/FormGroup/Validation.stories.js
@@ -6,9 +6,15 @@ import { linkTo } from '@storybook/addon-links';
 
 import {
   Button,
-  FormGroup, ControlLabel, HelpBlock, Checkbox, Radio, FormControl,
-  Container
-} from '../'
+  FormGroup,
+  ControlLabel,
+  HelpBlock,
+  Checkbox,
+  Radio,
+  FormControl,
+  Container,
+  Icon,
+} from '../';
 
 class FormExample extends React.Component {
   constructor(props) {
@@ -49,14 +55,53 @@ class FormExample extends React.Component {
           <FormControl.Feedback />
           <HelpBlock>Validation is based on string length.</HelpBlock>
         </FormGroup>
+        <div className="form-check abc-checkbox">
+          <input className="form-check-input" id="checkbox1" type="checkbox" />
+          <label className="form-check-label" htmlFor="checkbox1">
+            Check me out
+          </label>
+        </div>
+        <div className="pretty p-default">
+          <input type="checkbox" />
+          <div className="state p-primary">
+            <label>Check</label>
+          </div>
+        </div>
+        <div className="pretty p-icon p-smooth">
+          <input type="checkbox" />
+          <div className="state p-primary">
+            <i className="icon fa fa-check" />
+            {/* <Icon classNameName="icon" name="rocket" /> */}
+            <label>Fooo</label>
+          </div>
+        </div>
+
+        <div className="pretty p-default p-round p-bigger p-smooth">
+          <input type="radio" name="radio1" />
+          <div className="state p-primary">
+            <label>Male</label>
+          </div>
+        </div>
+
+        <div className="pretty p-default p-round p-bigger p-smooth">
+          <input type="radio" name="radio1" />
+          <div className="state p-primary">
+            <label>Female</label>
+          </div>
+        </div>
+
+        <div className="pretty p-default p-round p-bigger p-smooth">
+          <input type="radio" name="radio1" />
+          <div className="state p-primary">
+            <label>Special</label>
+          </div>
+        </div>
       </form>
     );
   }
 }
 
 storiesOf('Forms', module)
-
-
   /**
    *
    */
