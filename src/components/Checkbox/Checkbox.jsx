d8802eaa2c512d347bb681ab5324e8abb1be8e05
Danny Eck <daniel.eck@accenture.com>
2017-12-21 12:04:22 -0800

    Cleaner

--- src/components/Checkbox/Checkbox.jsx
+++ src/components/Checkbox/Checkbox.jsx
@@ -1,89 +1,214 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import warning from 'warning';
 import { Checkbox as CheckboxRBS, utils } from 'react-bootstrap';
 import { Icon } from '../';
 
 const { getClassSet, prefix, splitBsProps } = utils.bootstrapUtils;
 
-const propTypes = {
-  ...CheckboxRBS.propTypes,
-};
+const pretty = Component => {
+  return class Pretty extends Component {
+    static displayName = 'CWDS';
+    static propTypes = Component.propTypes;
+    static defaultProps = Component.defaultProps;
+    render() {
+      const {
+        inline,
+        disabled,
+        validationState,
+        inputRef,
+        className,
+        style,
+        title,
+        children,
+        ...props
+      } = this.props;
 
-const defaultProps = {
-  ...CheckboxRBS.propTypes,
-};
+      const [bsProps, elementProps] = splitBsProps(props);
 
-const Checkbox = ({
-  inline,
-  disabled,
-  validationState,
-  inputRef,
-  className,
-  style,
-  title,
-  children,
-  ...props
-}) => {
-  const [bsProps, elementProps] = splitBsProps(props);
-  const input = (
-    <input
-      {...elementProps}
-      ref={inputRef}
-      type="checkbox"
-      disabled={disabled}
-    />
-  );
+      const input = (
+        <input
+          {...elementProps}
+          ref={inputRef}
+          type="checkbox"
+          disabled={disabled}
+        />
+      );
 
-  if (inline) {
-    const classes = { [prefix(bsProps, 'inline')]: true, disabled };
-    return (
-      // <div className="pretty p-icon p-smooth">
-      //   <input type="checkbox" />
-      //   <div className="state p-primary">
-      //     <Icon className="icon" name="check" />
-      //     <label title={title}>{children}</label>
-      //   </div>
-      // </div>
-      <label
-        className={classNames(className, classes)}
-        style={style}
-        title={title}
-      >
-        {input}
-        {children}
-      </label>
-    );
-  }
+      if (inline) {
+        const classes = {
+          [prefix(bsProps, 'inline')]: true,
+          disabled,
+        };
 
-  const classes = {
-    ...getClassSet(bsProps),
-    disabled,
+        // Use a warning here instead of in propTypes to get better-looking
+        // generated documentation.
+        warning(
+          !validationState,
+          '`validationState` is ignored on `<Checkbox inline>`. To display ' +
+            'validation state on an inline checkbox, set `validationState` on a ' +
+            'parent `<FormGroup>` or other element instead.'
+        );
+
+        return (
+          // <label
+          //   className={classNames(className, classes)}
+          //   style={style}
+          //   title={title}
+          // >
+          //   {input}
+          //   {children}
+          // </label>
+          <div
+            className={classNames(
+              className,
+              classes,
+              'pretty',
+              'p-icon',
+              'p-smooth'
+            )}
+          >
+            {input}
+            <div className="state p-primary">
+              <Icon className="icon" name="check" />
+              <label>{children}</label>
+            </div>
+          </div>
+        );
+      }
+
+      const classes = {
+        ...getClassSet(bsProps),
+        disabled,
+      };
+      if (validationState) {
+        classes[`has-${validationState}`] = true;
+      }
+
+      return (
+        // <div className={classNames(className, classes)} style={style}>
+        //   <label title={title}>
+        //     {input}
+        //     {children}
+        //   </label>
+        // </div>
+        <div
+          className={classNames(
+            className,
+            classes,
+            'pretty',
+            'p-icon',
+            'p-smooth'
+          )}
+        >
+          {/* <input type="checkbox" /> */}
+          {input}
+          {/* <div className="state p-primary"> */}
+          <div
+            className={classNames('state', {
+              // 'p-primary': validationState
+              'p-success': 'success' === validationState,
+              // 'p-info':
+              'p-warning': 'warning' === validationState,
+              'p-danger': 'error' === validationState,
+            })}
+          >
+            <Icon className="icon" name="check" />
+            <label>{children}</label>
+          </div>
+        </div>
+      );
+    }
   };
-  if (validationState) {
-    classes[`has-${validationState}`] = true;
-  }
-
-  // return (
-  //   <div className={classNames(className, classes)} style={style}>
-  //     <label title={title}>
-  //       {input}
-  //       {children}
-  //     </label>
-  //   </div>
-  // );
-  return (
-    <div className="pretty p-icon p-smooth">
-      <input type="checkbox" />
-      <div className="state p-primary">
-        <Icon className="icon" name="check" />
-        <label>{children}</label>
-      </div>
-    </div>
-  );
 };
 
-Checkbox.propTypes = propTypes;
-Checkbox.defaultProps = defaultProps;
+export default pretty(CheckboxRBS);
 
-export default Checkbox;
+//
+//
+//
+
+// const { getClassSet, prefix, splitBsProps } = utils.bootstrapUtils;
+
+// const propTypes = {
+//   ...CheckboxRBS.propTypes,
+// };
+
+// const defaultProps = {
+//   ...CheckboxRBS.propTypes,
+// };
+
+// const Checkbox = ({
+//   inline,
+//   disabled,
+//   validationState,
+//   inputRef,
+//   className,
+//   style,
+//   title,
+//   children,
+//   ...props
+// }) => {
+//   const [bsProps, elementProps] = splitBsProps(props);
+//   const input = (
+//     <input
+//       {...elementProps}
+//       ref={inputRef}
+//       type="checkbox"
+//       disabled={disabled}
+//     />
+//   );
+
+//   if (inline) {
+//     const classes = { [prefix(bsProps, 'inline')]: true, disabled };
+//     return (
+//       // <div className="pretty p-icon p-smooth">
+//       //   <input type="checkbox" />
+//       //   <div className="state p-primary">
+//       //     <Icon className="icon" name="check" />
+//       //     <label title={title}>{children}</label>
+//       //   </div>
+//       // </div>
+//       <label
+//         className={classNames(className, classes)}
+//         style={style}
+//         title={title}
+//       >
+//         {input}
+//         {children}
+//       </label>
+//     );
+//   }
+
+//   const classes = {
+//     ...getClassSet(bsProps),
+//     disabled,
+//   };
+//   if (validationState) {
+//     classes[`has-${validationState}`] = true;
+//   }
+
+//   // return (
+//   //   <div className={classNames(className, classes)} style={style}>
+//   //     <label title={title}>
+//   //       {input}
+//   //       {children}
+//   //     </label>
+//   //   </div>
+//   // );
+//   return (
+//     <div className="pretty p-icon p-smooth">
+//       <input type="checkbox" />
+//       <div className="state p-primary">
+//         <Icon className="icon" name="check" />
+//         <label>{children}</label>
+//       </div>
+//     </div>
+//   );
+// };
+
+// Checkbox.propTypes = propTypes;
+// Checkbox.defaultProps = defaultProps;
+
+// export default Checkbox;
--- src/components/Checkbox/Checkbox.stories.js
+++ src/components/Checkbox/Checkbox.stories.js
@@ -6,6 +6,7 @@ import { linkTo } from '@storybook/addon-links';
 
 import { Checkbox as CheckboxRBS } from 'react-bootstrap';
 import Checkbox from './Checkbox';
+import { Container, Row, Col, FormGroup, Icon } from '../';
 
 storiesOf('Checkbox', module)
   /**
@@ -14,47 +15,139 @@ storiesOf('Checkbox', module)
   .add('duh', () => <Checkbox>Some Label</Checkbox>)
 
   .add('default', () => (
-    <div>
-      <div>
-        <Checkbox>One</Checkbox>
-        <Checkbox>Two</Checkbox>
-        <Checkbox>Three</Checkbox>
-        <Checkbox>Four</Checkbox>
-        <Checkbox>Five</Checkbox>
-      </div>
-      <div className="checkbox">
-        <label>
-          <input type="checkbox" value="" />
-          Option one is this and that&mdash;be sure to include why it's great
-        </label>
-      </div>
-      <div className="checkbox disabled">
-        <label>
-          <input type="checkbox" value="" disabled />
-          Option two is disabled
-        </label>
-      </div>
-      <h4>Default</h4>
-      <CheckboxRBS>One</CheckboxRBS>
-      <CheckboxRBS>Two</CheckboxRBS>
-      <CheckboxRBS>Three</CheckboxRBS>
-      <CheckboxRBS disabled>Four</CheckboxRBS>
+    <Container>
+      <Row className="text-center">
+        <Col md={4}>
+          <h4>Bootstrap3</h4>
+        </Col>
+        <Col md={4}>
+          <h4>React-Bootstrap</h4>
+        </Col>
+        <Col md={4}>
+          <h4>CWDS</h4>
+        </Col>
+      </Row>
+
+      <h4>Defaults</h4>
+      <Row>
+        <Col md={4}>
+          <div className="checkbox">
+            <label>
+              <input type="checkbox" value="" />
+              One
+            </label>
+          </div>
+          <div className="checkbox">
+            <label>
+              <input type="checkbox" value="" />
+              Two
+            </label>
+          </div>
+          <div className="checkbox disabled">
+            <label>
+              <input type="checkbox" value="" disabled />
+              Three (disabled)
+            </label>
+          </div>
+        </Col>
+        <Col md={4}>
+          <CheckboxRBS>One</CheckboxRBS>
+          <CheckboxRBS>Two</CheckboxRBS>
+          <CheckboxRBS disabled>Three</CheckboxRBS>
+        </Col>
+        <Col md={4}>
+          <Checkbox>One</Checkbox>
+          <Checkbox>Two</Checkbox>
+          <Checkbox disabled={true}>Three</Checkbox>
+        </Col>
+      </Row>
+
       <h4>
         with <tt>inline</tt>
       </h4>
-      <CheckboxRBS inline>One</CheckboxRBS>
-      <CheckboxRBS inline>Two</CheckboxRBS>
-      <CheckboxRBS inline>Three</CheckboxRBS>
-      <CheckboxRBS inline>Four</CheckboxRBS>
-      <h4>My Checkbox</h4>
-      <div>
-        <Checkbox inline={false}>One</Checkbox>
-        <Checkbox inline={false}>Two</Checkbox>
-        <Checkbox inline={false}>Three</Checkbox>
-        <Checkbox inline={false}>Four</Checkbox>
-        <Checkbox inline={false}>Five</Checkbox>
-      </div>
-    </div>
+      <Row>
+        <Col md={4}>
+          <label className="checkbox-inline">
+            <input type="checkbox" id="inlineCheckbox1" value="option1" /> 1
+          </label>
+          <label className="checkbox-inline">
+            <input type="checkbox" id="inlineCheckbox2" value="option2" /> 2
+          </label>
+          <label className="checkbox-inline">
+            <input type="checkbox" id="inlineCheckbox3" value="option3" /> 3
+          </label>
+        </Col>
+        <Col md={4}>
+          <CheckboxRBS inline>One</CheckboxRBS>
+          <CheckboxRBS inline>Two</CheckboxRBS>
+          <CheckboxRBS inline disabled>
+            Three
+          </CheckboxRBS>
+        </Col>
+        <Col md={4}>
+          <Checkbox inline>One</Checkbox>
+          <Checkbox inline>Two</Checkbox>
+          <Checkbox inline disabled={true}>
+            Three
+          </Checkbox>
+        </Col>
+      </Row>
+
+      <br />
+      <h4>Validation States</h4>
+      <Row>
+        <Col md={4}>
+          <div>
+            <div className="checkbox">
+              <label>
+                <input type="checkbox" id="checkboxSuccess" value="option1" />
+                No ValidationState
+              </label>
+            </div>
+          </div>
+          <div className="has-success">
+            <div className="checkbox">
+              <label>
+                <input type="checkbox" id="checkboxSuccess" value="option1" />
+                Checkbox with success
+              </label>
+            </div>
+          </div>
+          <div className="has-warning">
+            <div className="checkbox">
+              <label>
+                <input type="checkbox" id="checkboxWarning" value="option1" />
+                Checkbox with warning
+              </label>
+            </div>
+          </div>
+          <div className="has-error">
+            <div className="checkbox">
+              <label>
+                <input type="checkbox" id="checkboxError" value="option1" />
+                Checkbox with error
+              </label>
+            </div>
+          </div>
+        </Col>
+        <Col md={4}>
+          <CheckboxRBS validationState={null}>No ValidationState</CheckboxRBS>
+          <CheckboxRBS validationState="success">
+            Checkbox with success
+          </CheckboxRBS>
+          <CheckboxRBS validationState="warning">
+            Checkbox with warning
+          </CheckboxRBS>
+          <CheckboxRBS validationState="error">Checkbox with error</CheckboxRBS>
+        </Col>
+        <Col md={4}>
+          <Checkbox validationState={null}>No ValidationState</Checkbox>
+          <Checkbox validationState="success">Checkbox with success</Checkbox>
+          <Checkbox validationState="warning">Checkbox with warning</Checkbox>
+          <Checkbox validationState="error">Checkbox with error</Checkbox>
+        </Col>
+      </Row>
+    </Container>
   ))
 
   .add('inlined', () => (
